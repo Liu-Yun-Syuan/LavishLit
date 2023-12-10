09d16c2a23f793a72323321c3b54f1d70d971424
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-12-10 19:53:15 +0800

    add unique id to ussers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_10_112211) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_10_115113) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,9 +47,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_10_112211) do
     t.string "full_name"
     t.string "nick_name"
     t.string "slug"
+    t.string "unique_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
+    t.index ["unique_id"], name: "index_users_on_unique_id", unique: true
   end
 
   add_foreign_key "posts", "users"
